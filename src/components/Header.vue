1b7cd22ea1a8aef7e4359d4a3196ee1700e0ddce
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-07-14 15:02:56 +0200

    Improve header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,38 @@
 <template>
   <header class="shadow-lg">
-    <a href="#home" @click="activeTab = 'home'">
-      <img src="../assets/icon/lawnotation_logo_v2.svg" alt="" class="logo"
-    /></a>
-    <ul class="navItems">
-      <li :class="{ active: isActive }">
-        <a class="item" href="#home" @click="activeTab = 'home'">Home</a>
-      </li>
-      <li :class="{ active: Data }">
-        <a class="item" href="#data&software" @click="activeTab = 'data'"
-          >Data & software</a
-        >
-      </li>
-      <li :class="{ active: team }">
-        <a class="item" href="#team" @click="activeTab = 'team'">Team</a>
-      </li>
-      <li :class="{ active: contact }">
-        <a class="item" href="#contactus" @click="activeTab = 'contact'"
-          >Contact us</a
-        >
-      </li>
-    </ul>
+    <div class="container mx-auto flex justify-between px-4 py-2">
+      <a href="#home" @click="activeTab = 'home'">
+        <img src="../assets/icon/lawnotation_logo_v2.svg" alt="" class="logo"
+      /></a>
+      <ul class="navItems">
+        <li :class="{ active: isActive }">
+          <a class="item" href="#home" @click="activeTab = 'home'">Home</a>
+        </li>
+        <li :class="{ active: data }">
+          <a class="item" href="#data&software" @click="activeTab = 'data'"
+            >Data & software</a
+          >
+        </li>
+        <li :class="{ active: team }">
+          <a class="item" href="#team" @click="activeTab = 'team'">Team</a>
+        </li>
+        <li :class="{ active: contact }">
+          <a class="item" href="#contactus" @click="activeTab = 'contact'">Contact us</a>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
 <script setup>
-import { ref, computed, toRefs } from "vue";
+import { computed, toRefs } from "vue";
 const props = defineProps(["activeTab"]);
 const { activeTab } = toRefs(props);
-// const activeTab = ref("home");
 
 const isActive = computed(() => {
   return activeTab.value === "home";
 });
-const Data = computed(() => {
+const data = computed(() => {
   return activeTab.value === "data";
 });
 const team = computed(() => {
