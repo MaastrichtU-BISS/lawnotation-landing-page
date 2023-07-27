7c9ba741dcf54b3b55772fa5ffa38d6438db0d69
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-07-27 16:21:40 +0200

    Big cleanup

--- src/components/DataSoftware.vue
+++ src/components/DataSoftware.vue
@@ -1,93 +1,36 @@
 <template>
-  <section class="Data_section">
-    <div class="data-title">
-      <h1>Data & Software</h1>
-    </div>
-    <div class="Data_container">
-      <div class="datasets_annotation">
-        <div class="Datasets_contaier">
-          <h3>Datasets:</h3>
-          <h5>Case Law:</h5>
+  <section class="w-[60%] mt-8">
+    <h3 class="text-xl font-semibold">Data & Software</h3>
+    <ul>
+      <li>
+        <a href="https://pypi.org/project/cellar-extractor/">CJEU extractor</a>
+        <p>
+          Extracts case law of the Court of Justice of the European Union (CJEU). Data
+          source: CELLAR / EUR-Lex
+        </p>
+      </li>
 
-          <ul>
-            <li>
-              <a href="https://pypi.org/project/cellar-extractor/"
-                >CJEU extractor :</a
-              >
-              <p class="Description">
-                Extracts case law of the Court of Justice of the European Union
-                (CJEU). Data source: CELLAR / EUR-Lex
-              </p>
-            </li>
+      <li>
+        <a href="https://pypi.org/project/echr-extractor/">ECHR extracto</a>
+        <p>Extracts case law of the European Court of Human Rights (ECHR)</p>
+      </li>
 
-            <li>
-              <a href="https://pypi.org/project/echr-extractor/"
-                >ECHR extracto</a
-              >
-              <p class="Description">
-                Extracts case law of the European Court of Human Rights (ECHR)
-              </p>
-            </li>
-            <li>
-              <a href="https://pypi.org/project/rechtspraak-extractor/"
-                >Rechtspraak extractor</a
-              >
-              <p class="Description">
-                Extracts case law of the Netherlands (Rechtspraak)
-              </p>
-            </li>
-          </ul>
-        </div>
-        <!-- //end of the Datastets container -->
-      </div>
-      <div class="processing_storage">
-        <div class="processing-container">
-          <h3>Pre-Processing:</h3>
-          <ul>
-            <li>
-              <a href="https://lawnotation-demo.vercel.app/"
-                >Article splitter (EU)</a
-              >
-              <p class="Description">
-                Splits legislation of the European Commission into articles
-              </p>
-              <p class="Description">more pre-processing to follow</p>
-            </li>
-            <li>
-              <div class="Storage_container annotation_storge">
-                <h3>Annotation & Storage :</h3>
-                <p class="Description">Not available yet</p>
-              </div>
-            </li>
-          </ul>
-        </div>
+      <li>
+        <a href="https://pypi.org/project/rechtspraak-extractor/"
+          >Rechtspraak extractor</a
+        >
+        <p>Extracts case law of the Netherlands (Rechtspraak)</p>
+      </li>
+    </ul>
 
-        <!-- //end of the processing container -->
-      </div>
-    </div>
+    <h3 class="text-xl font-semibold mt-4">Pre-Processing</h3>
+    <ul>
+      <li>
+        <a href="https://lawnotation-demo.vercel.app/">Article splitter (EU)</a>
+        <p>
+          Splits legislation of the European Commission into articles
+        </p>
+      </li>
+    </ul>
   </section>
 </template>
-
-<script setup>
-// import { onScrollIntersect } from "../composables/onScrollIntersect";
-// import { ref, onMounted, onUnmounted, defineEmits } from "vue";
-
-// const observer = ref({});
-// const scrollRef = ref({});
-
-// const emit = defineEmits(["data"]);
-
-// function onEnter(entry) {
-//   console.log("Entering", entry);
-//   emit("data");
-// }
-// // When the component is mounted, start observing
-// onMounted(() => {
-//   observer.value = onScrollIntersect(scrollRef.value, onEnter);
-// });
-
-// // When the component is removed, disconnect the observer (clean-up step)
-// onUnmounted(() => {
-//   observer.value.disconnect();
-// });
-</script>
