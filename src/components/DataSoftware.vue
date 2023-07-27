3b5bb5ac86ee9c7d43c3cb1d403809a4f111e5fb
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-07-27 16:26:42 +0200

    Improved styling

--- src/components/DataSoftware.vue
+++ src/components/DataSoftware.vue
@@ -11,7 +11,7 @@
       </li>
 
       <li>
-        <a href="https://pypi.org/project/echr-extractor/">ECHR extracto</a>
+        <a href="https://pypi.org/project/echr-extractor/">ECHR extractor</a>
         <p>Extracts case law of the European Court of Human Rights (ECHR)</p>
       </li>
 
@@ -23,7 +23,7 @@
       </li>
     </ul>
 
-    <h3 class="text-xl font-semibold mt-4">Pre-Processing</h3>
+    <h3 class="text-xl font-semibold mt-8">Pre-Processing</h3>
     <ul>
       <li>
         <a href="https://lawnotation-demo.vercel.app/">Article splitter (EU)</a>
@@ -34,3 +34,15 @@
     </ul>
   </section>
 </template>
+
+<style scoped>
+a {
+  text-decoration-line: underline;
+}
+li {
+  margin-top: 1.5rem;
+}
+li:first-of-type {
+  margin-top: 1rem;
+}
+</style>
