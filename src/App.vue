a44617002ecfd3e624e854334e1347cae72a12ea
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2023-07-27 16:31:38 +0200

    IntroTitle -> Title

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <Header :active-tab="activeTab"/>
 
   <main>
-    <IntroTitle @home="activeTab = 'home'"/>
+    <Title @home="activeTab = 'home'"/>
     <Intro  @data="activeTab = 'data'"/>
     <Team @team="activeTab = 'team'" />
   </main>
@@ -16,7 +16,7 @@ import Team from "./components/Team.vue";
 import Header from "./components/Header.vue";
 import Intro from "./components/Intro.vue";
 import Footer from "./components/Footer.vue";
-import IntroTitle from "./components/IntroTitle.vue";
+import Title from "./components/Title.vue";
 
 import { ref } from "vue";
 const activeTab = ref("home");
