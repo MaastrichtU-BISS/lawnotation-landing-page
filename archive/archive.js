40c784298d9f1b47b5962ac9a74890edf3b69c23
Carlos <carloseam94@gmail.com>
2024-07-04 12:30:23 +0200

    feat: add new page archive

---
+++ archive/archive.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import SearchDocuments from "../src/components/SearchDocuments.vue";
+import '../src/assets/main.css'
+
+const Vue = createApp(SearchDocuments);
+Vue.mount('#app');
