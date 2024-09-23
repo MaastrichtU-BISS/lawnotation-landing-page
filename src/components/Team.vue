12ef26c54d3cc06bd273aecf7c890a1e5eb130ef
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2024-09-23 14:22:32 +0200

    refactor: Abstract team member cards to own component

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,141 +1,61 @@
 <template>
   <section id="team" class="develpoer_team my-32" ref="scrollRef">
     <!-- <a ></a> -->
-    <span  class="team_title">
+    <span class="team_title">
       <h2 class="underline underline-offset-[5px]">Team</h2>
     </span>
-    <div class="card">
-      <div class="member-container">
-        <div class="member-img">
-          <img src="../assets/img/gijs.jpg" alt="" class="profile-photo" />
-        </div>
-        <div class="member-info">
-          <h4>
-            <a href="https://www.biss-institute.com/team/Gijs-van-Dijck/"
-              >Prof. Gijs van Dijck
-            </a>
-          </h4>
-          <p>Law, AI</p>
-          <p>
-            <a
-              href="https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab"
-            >
-              Maastricht Law and Tech Lab</a
-            >
-          </p>
-          <p>
-            <a href="https://www.biss-institute.com/"
-              >Brightlands Institute for Smart Society</a
-            >
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="member-container">
-        <div class="member-img">
-          <img src="../assets/img/carlos.jpg" alt="" class="profile-photo" />
-        </div>
-        <div class="member-info">
-          <h4>
-            <a href="https://www.biss-institute.com/team/Carlos-Aguilera/"
-              >Carlos Aguilera</a
-            >
-          </h4>
-          <p>data engineering, front-end development, back-end development</p>
-          <p>
-            <a
-              href="https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab"
-            >
-              Maastricht Law and Tech Lab</a
-            >
-          </p>
-          <p>
-            <a href="https://www.biss-institute.com/"
-              >Brightlands Institute for Smart Society</a
-            >
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="member-container">
-        <div class="member-img">
-          <img src="../assets/img/chris.jpg" alt="" class="profile-photo" />
-        </div>
-        <div class="member-info">
-          <h4>
-            <a href="https://www.biss-institute.com/team/chris-van-der-lans/"
-              >Chris van der Lans</a
-            >
-          </h4>
-          <p>
-            front-end development, user experience (UX), information architecture,
-            human-centric design
-          </p>
-
-          <p>
-            <a href="https://www.biss-institute.com/"
-              >Brightlands Institute for Smart Society</a
-            >
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <div class="card">
-      <div class="member-container">
-        <div class="member-img">
-          <img src="../assets/img/shashank.jpg" alt="" class="profile-photo" />
-        </div>
-        <div class="member-info">
-          <h4>
-            <a href="https://www.biss-institute.com/team/Shashank-Chakravarthy/"
-              >Shashank Chakravarthy</a
-            >
-          </h4>
-          <p>data engineering, NLP, machine learning, AI</p>
-          <p>
-            <a
-              href="https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab"
-            >
-              Maastricht Law and Tech Lab</a
-            >
-          </p>
-          <p>
-            <a href="https://www.biss-institute.com/"
-              >Brightlands Institute for Smart Society</a
-            >
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="member-container">
-        <div class="member-img">
-          <img src="../assets/img/sander.jpg" alt="" class="profile-photo" />
-        </div>
-        <div class="member-info">
-          <h4>
-            <a href="https://www.biss-institute.com/team/sander-van-essel/"
-              >Sander van Essel</a
-            >
-          </h4>
-          <p>Frontend development, backend development, data engineering</p>
-          <p>
-            <a href="https://www.biss-institute.com/"
-              >Brightlands Institute for Smart Society</a
-            >
-          </p>
-        </div>
-      </div>
-    </div>
+    <TeamMember name="Prof. Gijs van Dijck" link="https://www.biss-institute.com/team/Gijs-van-Dijck/"
+      image="src/assets/img/gijs.jpg">
+      <p>Law, AI</p>
+      <p>
+        <a href="https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab">
+          Maastricht Law and Tech Lab</a>
+      </p>
+      <p>
+        <a href="https://www.biss-institute.com/">Brightlands Institute for Smart Society</a>
+      </p>
+    </TeamMember>
+    <TeamMember name="Carlos Aguilera" link="https://www.biss-institute.com/team/Carlos-Aguilera/"
+      image="src/assets/img/carlos.jpg">
+      <p>data engineering, front-end development, back-end development</p>
+      <p>
+        <a href="https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab">
+          Maastricht Law and Tech Lab</a>
+      </p>
+      <p>
+        <a href="https://www.biss-institute.com/">Brightlands Institute for Smart Society</a>
+      </p>
+    </TeamMember>
+    <TeamMember name="Chris van der Lans" link="https://www.biss-institute.com/team/chris-van-der-lans/"
+      image="src/assets/img/chris.jpg">
+      <p> front-end development, user experience (UX), information architecture, human-centric design </p>
+      <p><a href="https://www.biss-institute.com/">Brightlands Institute for Smart Society</a></p>
+    </TeamMember>
+    <TeamMember name="Shashank Chakravarthy" link="https://www.biss-institute.com/team/Shashank-Chakravarthy/"
+      image="src/assets/img/shashank.jpg">
+      <p>data engineering, NLP, machine learning, AI</p>
+      <p>
+        <a href="https://www.maastrichtuniversity.nl/about-um/faculties/law/research/law-and-tech-lab">
+          Maastricht Law and Tech Lab</a>
+      </p>
+      <p>
+        <a href="https://www.biss-institute.com/">Brightlands Institute for Smart Society</a>
+      </p>
+    </TeamMember>
+    <TeamMember name="Sander van Essel" link="https://www.biss-institute.com/team/sander-van-essel/"
+      image="src/assets/img/sander.jpg">
+      <p>Frontend development, backend development, data engineering</p>
+      <p>
+        <a href="https://www.biss-institute.com/">Brightlands Institute for Smart Society</a>
+      </p>
+    </TeamMember>
   </section>
 </template>
 
 <script setup>
 import { onScrollIntersect } from "../composables/onScrollIntersect";
 import { ref, onMounted, onUnmounted } from "vue";
+import TeamMember from "./TeamMember.vue";
 
 const observer = ref({});
 const scrollRef = ref({});
---
+++ src/components/TeamMember.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="member-container bg-beige">
+            <div class="member-img">
+                <img :src="image" alt="" class="profile-photo" />
+            </div>
+            <div class="member-info">
+                <h4>
+                    <a :href="link">{{ name }}</a>
+                </h4>
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { image, link, name } = defineProps({
+    image: String,
+    link: String,
+    name: String,
+});
+</script>
