560cc8729b58b52942aca5597f9ea6c68d745d34
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2024-09-23 14:57:43 +0200

    style: Add pins to notes and extract code to component

---
+++ src/components/Note.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="`bg-beige p-6 rounded-lg shadow-md flex flex-col items-start transform rotate-[${rotate}deg]`">
+        <slot></slot>
+        <h2 class="font-bold text-lg mb-2">{{ title }}</h2>
+        <p class="text-sm">{{ text }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { title, text } = defineProps({
+    rotate: {
+        type: Number,
+        default: 0,
+    },
+    title: String,
+    text: String,
+})
+</script>
---
+++ src/components/Pin.vue
@@ -0,0 +1,37 @@
+<template>
+    <svg width="16" height="27" viewBox="0 0 16 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_i_481_52)">
+            <rect x="6" y="15" width="4" height="12" fill="#AAAAAA" />
+        </g>
+        <g filter="url(#filter1_i_481_52)">
+            <circle cx="8" cy="8" r="8" fill="#F25574" />
+        </g>
+        <defs>
+            <filter id="filter0_i_481_52" x="6" y="15" width="4" height="16" filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                    result="hardAlpha" />
+                <feOffset dy="4" />
+                <feGaussianBlur stdDeviation="2" />
+                <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                <feBlend mode="normal" in2="shape" result="effect1_innerShadow_481_52" />
+            </filter>
+            <filter id="filter1_i_481_52" x="0" y="0" width="16" height="20" filterUnits="userSpaceOnUse"
+                color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                    result="hardAlpha" />
+                <feOffset dy="4" />
+                <feGaussianBlur stdDeviation="2" />
+                <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                <feColorMatrix type="matrix"
+                    values="0 0 0 0 0.223568 0 0 0 0 0.00931532 0 0 0 0 0.0471246 0 0 0 0.4 0" />
+                <feBlend mode="normal" in2="shape" result="effect1_innerShadow_481_52" />
+            </filter>
+        </defs>
+    </svg>
+</template>
