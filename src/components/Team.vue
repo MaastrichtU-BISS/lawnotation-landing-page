5b4b7708b3821ea8ec3296b285254e72068761e9
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2025-02-21 15:38:03 +0100

    feat: Underline svg arrow as well

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -42,10 +42,13 @@
         </a>
       </p>
       <p>
-        <a class="hover:underline" href="https://www.biss-institute.com/">Brightlands Institute for Smart Society
+        <a class="group hover:underline" href="https://www.biss-institute.com/">
+          Brightlands Institute for Smart Society
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="h-4 inline">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            <text class="stroke-transparent group-hover:stroke-current" font-size="4px" text-anchor="middle"
+              dominant-baseline="central" y="18" x="12">__________</text>
           </svg>
         </a>
       </p>
